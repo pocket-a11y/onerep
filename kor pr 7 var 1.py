5d12dae36c1882808ee3c310a80bbf1d0b4aa5fc
pocket-a11y <koroteev228337@gmail.com>
2024-10-13 17:54:41 +0300

    Add files via upload

---
+++ kor pr 7 var 1.py
@@ -0,0 +1,7 @@
+A = int(input("Введите число А "))
+B = int(input("Введите число В "))
+H = int(input("Введите число H "))
+S1=A**2
+S2=A*B
+S3=0.5*H*A
+print(S1,S2,S3)
---
+++ kor pr6 var 1.py
@@ -0,0 +1,5 @@
+a=[8,25,99,52,3,31,0]
+a.reverse()
+print(a)
+b=max(a)
+print(b)
